866c1fe5c7996534c8e5acd48d24daaafc498e3a
Richie Adams <richieladams@gmail.com>
2016-02-24 16:54:13 -0500

    Final State PID Values

--- PID_ValuesMotorRPM.h
+++ PID_ValuesMotorRPM.h
@@ -1,5 +1,5 @@
-int rpmLow = 104;
-int rpmMid = 140;
+int rpmLow = 102;
+int rpmMid = 120;
 int rpmHigh = 158;
 //                 PID CONSTANTS                       //
 /////////////////////////////////////////////////////////
@@ -10,50 +10,20 @@ float KiR = 1.0;
 float KdL = 1.0;
 float KdR = 1.0;
 /////////////////////////////////////////////////////////
-/*PID Tuner
-p = .0235;
-i = .000007
-d= .42
-*/
-float KpLowShooting = 0.0205454545;
-float KiLowShooting = 0.00115;//0.002103;//0.002103;//0.0007
-float KdLowShooting = 0.5434;//0.7572413800000;//0.7572413800000;//0.0021
-float KpLow = 0.000900500;
-float KiLow = 0.000000200;
-float KdLow = 0.005000000;
+float KpLowShooting = 0.0675;
+float KiLowShooting = 0.00675;//0.002103;//0.002103;//0.0007
+float KdLowShooting = 0.6753;//0.7572413800000;//0.7572413800000;//0.0021
+float KpLow = 0.01900500;
+float KiLow = 0.00000200;
+float KdLow = 0.05000000;
 /////////////////////////////////////////////////////////
-float KpMid = 0.0160000;
-float KiMid = 0.00240;//0.00050;
-float KdMid = 0.002;//0.008;
+float KpMid = 0.018;
+float KiMid = 0.00350;//0.00050;
+float KdMid = 0.4;//0.008;
 /////////////////////////////////////////////////////////
 float KpHighRS = 0.01600000; // 0.10
 float KiHighRS = 0.0024000; // 0.0024
 float KdHighRS  = 0.002000; // 0.0048
-
-/* final mateo values
-float KpHighRS = 0.010600000;
-float KiHighRS = 0.00240;
-float KdHighRS  = 0.0030000;
-
-/*
-most bad shots shoot just a bit too strong, but every once in a while there is a short shot
-raise the d a little more to correct overshot, and eitehr raise the p or i to fix the every once in a while short shots
-/*
-BEST
-float KpHighRS = 0.021000000;//.008
-float KiHighRS = 0.00202;//.0007
-float KdHighRS  = 0.0042000;// .009//.00155
-
-float KpHighRS = 0.018000000;//.008
-float KiHighRS = 0.000200;//.00202
-float KdHighRS  = 0.004040000;// .009//.00155*/
-
-/* good with 1575/1582
-float KpHighRS = 0.020000000;//.008
-float KiHighRS = 0.00202;//.0007
-float KdHighRS  = 0.000042000;// .009//.00155
-*/
-
 float KpHighLS = KpHighRS;
 float KiHighLS = KiHighRS;
 float KdHighLS = KdHighRS;
@@ -81,31 +51,6 @@ float KdMidSoft = 0.5;//0.008;
 float KpHighRSSoft = 0.01600000; // 0.10
 float KiHighRSSoft = 0.0024000; // 0.0024
 float KdHighRSSoft  = 0.002000; // 0.0048
-
-/* final mateo values
-float KpHighRS = 0.010600000;
-float KiHighRS = 0.00240;
-float KdHighRS  = 0.0030000;
-
-/*
-most bad shots shoot just a bit too strong, but every once in a while there is a short shot
-raise the d a little more to correct overshot, and eitehr raise the p or i to fix the every once in a while short shots
-/*
-BEST
-float KpHighRS = 0.021000000;//.008
-float KiHighRS = 0.00202;//.0007
-float KdHighRS  = 0.0042000;// .009//.00155
-
-float KpHighRS = 0.018000000;//.008
-float KiHighRS = 0.000200;//.00202
-float KdHighRS  = 0.004040000;// .009//.00155*/
-
-/* good with 1575/1582
-float KpHighRS = 0.020000000;//.008
-float KiHighRS = 0.00202;//.0007
-float KdHighRS  = 0.000042000;// .009//.00155
-*/
-
 float KpHighLSSoft = KpHighRSSoft;
 float KiHighLSSoft = KiHighRSSoft;
 float KdHighLSSoft = KdHighRSSoft;
